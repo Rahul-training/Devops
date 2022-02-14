53ef3f54040de081645d17c1746b200d4e894958
Rahul-training <52369556+Rahul-training@users.noreply.github.com>
2022-02-14 13:12:38 +0530

    Add files via upload

---
+++ terraform/main.tf
@@ -0,0 +1,141 @@
+resource "azurerm_resource_group" "myterraformgroup" {
+    name     = "myResourceGroup"
+    location = "eastus"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+# Create virtual network
+resource "azurerm_virtual_network" "myterraformnetwork" {
+    name                = "myVnet"
+    address_space       = ["10.0.0.0/16"]
+    location            = "eastus"
+    resource_group_name = azurerm_resource_group.myterraformgroup.name
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+# Create subnet
+resource "azurerm_subnet" "myterraformsubnet" {
+    name                 = "mySubnet"
+    resource_group_name  = azurerm_resource_group.myterraformgroup.name
+    virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
+    address_prefixes       = ["10.0.1.0/24"]
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "myterraformpublicip" {
+    name                         = "myPublicIP"
+    location                     = "eastus"
+    resource_group_name          = azurerm_resource_group.myterraformgroup.name
+    allocation_method            = "Dynamic"
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "myterraformnsg" {
+    name                = "myNetworkSecurityGroup"
+    location            = "eastus"
+    resource_group_name = azurerm_resource_group.myterraformgroup.name
+
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*"
+    }
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+# Create network interface
+resource "azurerm_network_interface" "myterraformnic" {
+    name                      = "myNIC"
+    location                  = "eastus"
+    resource_group_name       = azurerm_resource_group.myterraformgroup.name
+
+    ip_configuration {
+        name                          = "myNicConfiguration"
+        subnet_id                     = azurerm_subnet.myterraformsubnet.id
+        private_ip_address_allocation = "Dynamic"
+        public_ip_address_id          = azurerm_public_ip.myterraformpublicip.id
+    }
+
+    tags = {
+        environment = "Terraform Demo"
+    }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+    network_interface_id      = azurerm_network_interface.myterraformnic.id
+    network_security_group_id = azurerm_network_security_group.myterraformnsg.id
+}
+
+# Create virtual machine
+resource "azurerm_linux_virtual_machine" "myterraformvm" {
+    name                  = "myVM"
+    location              = "eastus"
+    resource_group_name   = azurerm_resource_group.myterraformgroup.name
+    network_interface_ids = [azurerm_network_interface.myterraformnic.id]
+    size                  = "Standard_A2_v2"
+
+    os_disk {
+        name              = "myOsDisk"
+        caching           = "ReadWrite"
+        storage_account_type = "StandardSSD_LRS"
+    }
+
+    source_image_reference {
+        publisher = "Canonical"
+        offer     = "UbuntuServer"
+        sku       = "18.04-LTS"
+        version   = "latest"
+    }
+
+    computer_name  = "myvm"
+    admin_username = "azureuser"
+    admin_password = "Windows@123456"
+    disable_password_authentication = false
+
+     tags = {
+        environment = "Terraform Demo"
+    }
+    connection {
+        host = self.public_ip_address
+        user = "azureuser"
+        password= "Windows@123456"
+        type = "ssh"
+    }
+
+    provisioner "remote-exec" {
+        inline = [
+          "sudo apt-get update",
+          "sudo apt-get install curl",
+          "curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl",
+          "chmod +x kubectl",
+          "sudo mv kubectl /usr/local/bin",
+          "sudo apt-get install docker.io -y",
+          "sudo gpasswd -a $USER docker && newgrp docker",
+          "wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
+          "chmod +x minikube-linux-amd64",
+          "sudo mv minikube-linux-amd64 /usr/local/bin/minikube",
+          "minikube version",
+          "sudo apt-get install conntrack -y",
+          "sudo minikube start --vm-driver=none",
+          "kubectl cluster-info"
+        ]
+    }    
+}    
