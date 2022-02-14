52ebb93cb0e5d05e83ea81164c58307cf19cd384
Rahul-training <52369556+Rahul-training@users.noreply.github.com>
2022-02-14 13:36:04 +0530

    Add files via upload

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
     
-    subscription_id = "0cee2ddb-0de2-4f95-868d-7fafda0f945e"
-    tenant_id = "6dca9d3f-35ef-4279-a25a-630be5066ad5"
-    client_id =  "33d72890-ead2-42d5-9cd1-298472cef097"
-    client_secret = ".mA7Q~wvt4.JIaYPLibcUKkoXk1USVQmNYFA5"
+    subscription_id = var.subscription_id
+    tenant_id = var.tenant_id
+    client_id = var.client_id
+    client_secret = var.client_secret
     features {}
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,30 @@
+variable subscription_id {
+    type = string
+    description = "Azure Subscription Id"
+}
+
+variable tenant_id {
+    type = string
+    description = "Azure Tenant Id"
+}
+
+variable location {
+    type = string
+    description = "Resource Group Location"
+}
+
+variable group_name {
+    type = string
+    description = "Resource Group Name"
+}
+
+
+variable client_id {
+    type = string
+    description = "Client Id"
+}
+
+variable client_secret {
+    type = string
+    description = "Client Secret"
+}
