1c302d63a41df51683ec32aad0eab6418ed662e0
Rahul-training <52369556+Rahul-training@users.noreply.github.com>
2022-02-14 14:13:09 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,11 @@
+provider "azurerm" {
+    
+    subscription_id = var.subscription_id
+    tenant_id = var.tenant_id
+    client_id = var.client_id
+    client_secret = var.client_secret
+    features {}
+}
 resource "azurerm_resource_group" "rg" {
     name = var.group_name
     location = var.location
