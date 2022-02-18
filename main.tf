00d2b0ff900177463c464e2216484a68e5e2e299
Rahul-training <52369556+Rahul-training@users.noreply.github.com>
2022-02-18 14:11:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "azurerm_public_ip" "myterraformpublicip" {
     name                         = "myPublicIP"
     location                     = "eastus"
     resource_group_name          = azurerm_resource_group.myterraformgroup.name
-    allocation_method            = "Dynamic"
+    allocation_method            = "Static"
 
     tags = {
         environment = "Terraform Demo"
@@ -50,7 +50,7 @@ resource "azurerm_network_security_group" "myterraformnsg" {
         access                     = "Allow"
         protocol                   = "Tcp"
         source_port_range          = "*"
-        destination_port_range     = "22"
+        destination_port_ranges     = ["22", "80", "443", "32323"]
         source_address_prefix      = "*"
         destination_address_prefix = "*"
     }
@@ -112,21 +112,26 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
 
      tags = {
         environment = "Terraform Demo"
-    }
-  connection {
+    } 
+    connection {
         host = self.public_ip_address
         user = "azureuser"
         password= "Windows@123456"
         type = "ssh"
     }
 
+    provisioner "file" {
+    source      = "script.sh"
+    destination = "script.sh"
+  }
+
     provisioner "remote-exec" {
         inline = [
           "sudo apt-get update",
           "sudo apt-get install curl",
           "curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl",
-          "chmod +x kubectl",
-          "sudo mv kubectl /usr/local/bin",
+          "chmod +x ./kubectl",
+          "sudo mv ./kubectl /usr/local/bin/kubectl",
           "sudo apt-get install docker.io -y",
           "sudo gpasswd -a $USER docker",
           "wget https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64",
@@ -134,7 +139,15 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
           "sudo mv minikube-linux-amd64 /usr/local/bin/minikube",
           "minikube version",
           "sudo apt-get install conntrack -y",
-          "sudo minikube start --vm-driver=none"
+          "sudo minikube start --vm-driver=none",
+          "sudo kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.5.0/aio/deploy/recommended.yaml",
+          "sudo kubectl create serviceaccount admin-user --namespace kubernetes-dashboard",
+          "sudo kubectl create clusterrolebinding admin-user --clusterrole=cluster-admin --serviceaccount=kubernetes-dashboard:admin-user",
+          "chmod +x script.sh",
+          "sh script.sh",
+          "rm -rf service.yml",
+          "rm -rf script.sh",
+          "cat token.txt"
         ]
     }  
-}    
+}
